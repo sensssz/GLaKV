f61a54c9ecc80849dd9540c7cfdb4a596222bf93
sensssz <hjmsens@gmail.com>
2016-05-16 14:45:52 -0400

    Add submit_task to pass by ref

--- src/thread_pool.cc
+++ src/thread_pool.cc
@@ -22,3 +22,7 @@ thread_pool::~thread_pool() {
 void thread_pool::submit_task(task &&db_task) {
     task_queue.enqueue(db_task);
 }
+
+void thread_pool::submit_task(task &db_task) {
+    task_queue.enqueue(db_task);
+}
