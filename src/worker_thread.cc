67d51f216aa4bffa98008db3fca34bd4a9750b7f
sensssz <hjmsens@gmail.com>
2016-05-16 22:51:55 -0400

    Measure time in microseconds

--- src/worker_thread.cc
+++ src/worker_thread.cc
@@ -51,7 +51,7 @@ void worker_thread::start() {
             if (db_task.operation != fetch ||
                 db_task.operation != noop) {
                 auto end = std::chrono::high_resolution_clock::now();
-                auto diff = end - db_task.birth_time;
+                auto diff = std::chrono::duration_cast<microseconds>(end - db_task.birth_time);
                 db_task.callback(success, val, diff.count());
             }
         }
