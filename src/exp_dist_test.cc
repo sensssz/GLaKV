2d2073ace04f6923efa7a3dec2fe4ad03ef697e2
sensssz <hjmsens@gmail.com>
2016-05-19 16:47:18 -0400

    Change test and use map

--- src/exp_dist_test.cc
+++ src/exp_dist_test.cc
@@ -7,30 +7,30 @@
 
 #include <iostream>
 #include <string>
-#include <unordered_map>
+#include <map>
 #include <iomanip>
 #include <map>
 
 using std::cout;
 using std::endl;
 using std::string;
-using std::unordered_map;
+using std::map;
 
 int main(int argc, char *argv[]) {
     double lambda = 1;
     if (argc == 2) {
         lambda = atof(argv[1]);
     }
-    unordered_map<uint64_t, uint64_t> map;
+    map<uint64_t, uint64_t> hist;
     exponential_distribution distribution(lambda, 100);
 
     for (uint64_t count = 0; count < 100000; ++count) {
         uint64_t val = distribution.next();
-        map[val]++;
+        hist[val]++;
     }
 
-    for (auto iter : map) {
-        if (iter.second / 100 > 0) {
+    for (auto iter : hist) {
+        if (iter.second / 1000 > 0) {
             string val(iter.second / 100, '*');
             cout << iter.first << ": " << val << endl;
         }
