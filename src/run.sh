c2a1f5a8b39adedcbf326ac4fc537d1463182aed
sensssz <hjmsens@gmail.com>
2016-05-19 18:05:06 -0400

    Fix run.sh

--- src/run.sh
+++ src/run.sh
@@ -7,7 +7,7 @@ db_path=/home/jiamin/speculative/GLaKV/src
 output_path=/home/jiamin/speculative/out
 
 num_exp=2000
-exp_name="diff_c_p"
+exp_name="diff_m_c_p"
 
 if [[ $# -eq 1 ]]; then
     num_exp=$1
@@ -29,7 +29,7 @@ do
             fi
             ssh salat3 /bin/zsh << EOF
             export LD_LIBRARY_PATH=/home/jiamin/gcc/lib64:/home/jiamin/usr/lib:$LD_LIBRARY_PATH
-            echo -n "${p},${c}," >> ${output_path}/${exp_name}
+            echo -n "${m},${c},${p}," >> ${output_path}/${exp_name}
             ${db_path}/glakv_server --dir ${db_path}/glakv_home -p ${p} -n 1 >> ${output_path}/${exp_name}&
 EOF
             sleep 2
