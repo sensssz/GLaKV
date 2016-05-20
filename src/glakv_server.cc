a53c8b548b8991b9383a5ecae135a66b00721c5c
sensssz <hjmsens@gmail.com>
2016-05-19 20:04:47 -0400

    Show if prefetch is timely

--- src/glakv_server.cc
+++ src/glakv_server.cc
@@ -173,6 +173,14 @@ bool check_prefetch_cache(uint32_t key, unordered_map<uint32_t, string> &prefetc
 //    cout << "Checking prefetch cache" << endl;
     auto iter = prefetch_cache.find(key);
     if (iter == prefetch_cache.end()) {
+        if (prefetch_cache.size() > 0) {
+            cout << "Key is " << key << endl;
+            for (auto pair : prefetch_cache) {
+                cout << pair.first << ": " << pair.second << endl;
+            }
+        } else {
+            cout << "Prefetch is taking too much time" << endl;
+        }
         prefetch_cache.clear();
         return false;
     }
