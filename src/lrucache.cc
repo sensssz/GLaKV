1fc026a316c5c0db6340c3d452d0cc1d9bc3d7b8
sensssz <hjmsens@gmail.com>
2016-05-20 13:20:01 -0400

    Unlock read_lock before upgrade Otherwise it will deadlock

--- src/lrucache.cc
+++ src/lrucache.cc
@@ -40,6 +40,7 @@ bool LRUCache::get(uint32_t key, string &val) {
     auto map_iter = cache.find(key);
     if (map_iter != cache.end()) {
         val = cache[key].first;
+        read_lock.unlock();
         upgrade_lock<shared_mutex> lock(mutex);
         upgrade_to_unique_lock<shared_mutex> write_lock(lock);
         lru_keys.erase(map_iter->second.second);
