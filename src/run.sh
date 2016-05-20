12fdd81bb17305c668dff5c0c0061ed756a0533a
sensssz <hjmsens@gmail.com>
2016-05-20 13:23:07 -0400

    Change run.sh

--- src/run.sh
+++ src/run.sh
@@ -33,12 +33,12 @@ do
                 export LD_LIBRARY_PATH=/home/jiamin/gcc/lib64:/home/jiamin/usr/lib:$LD_LIBRARY_PATH
                 echo -n "${t},${m},${c},${p}," >> ${output_path}/${exp_name}
                 ${db_path}/glakv_server --dir ${db_path}/glakv_home -p ${p} -n 1 -t ${t} >> ${output_path}/${exp_name}&
-                if [[ ! $? -eq 0 ]]; then
-                    echo '' >> ${output_path}/${exp_name}
-                fi
 EOF
                 sleep 2
                 ${db_path}/glakv_client -e -m ${m} -c ${c} -n ${num_exp}
+                if [[ $? -ne 0 ]]; then
+                    ssh salat3 echo '' >> ${output_path}/${exp_name}
+                fi
                 sleep 1
             done
         done
