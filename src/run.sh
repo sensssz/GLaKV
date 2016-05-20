8f0349363976795dea9146aa2eb4946bd4806e14
sensssz <hjmsens@gmail.com>
2016-05-20 11:32:13 -0400

    Change number of worker threads

--- src/run.sh
+++ src/run.sh
@@ -18,23 +18,26 @@ trap 'quit=1' INT
 ssh salat3 "mkdir -p ${output_path}/"
 ssh salat3 "rm ${output_path}/${exp_name} && touch ${output_path}/${exp_name}"
 
-for m in 0.15 0.2 0.25;
+for t in 10 20 30;
 do
-    for((c=64;c<=256;c*=2));
+    for m in 0.15 0.2 0.25;
     do
-        for p in `seq 0 5`;
+        for((c=64;c<=256;c*=2));
         do
-            if [[ ! -z ${quit+x} ]]; then
-                exit 0
-            fi
-            ssh salat3 /bin/zsh << EOF
-            export LD_LIBRARY_PATH=/home/jiamin/gcc/lib64:/home/jiamin/usr/lib:$LD_LIBRARY_PATH
-            echo -n "${m},${c},${p}," >> ${output_path}/${exp_name}
-            ${db_path}/glakv_server --dir ${db_path}/glakv_home -p ${p} -n 1 >> ${output_path}/${exp_name}&
-EOF
-            sleep 2
-            ${db_path}/glakv_client -e -m ${m} -c ${c} -n ${num_exp}
-            sleep 1
+            for p in `seq 0 5`;
+            do
+                if [[ ! -z ${quit+x} ]]; then
+                    exit 0
+                fi
+                ssh salat3 /bin/zsh << EOF
+                export LD_LIBRARY_PATH=/home/jiamin/gcc/lib64:/home/jiamin/usr/lib:$LD_LIBRARY_PATH
+                echo -n "${m},${c},${p}," >> ${output_path}/${exp_name}
+                ${db_path}/glakv_server --dir ${db_path}/glakv_home -p ${p} -n 1 >> ${output_path}/${exp_name}&
+    EOF
+                sleep 2
+                ${db_path}/glakv_client -e -m ${m} -c ${c} -n ${num_exp}
+                sleep 1
+            done
         done
     done
 done
