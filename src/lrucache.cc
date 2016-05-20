a309dde64af6d2dc4fd79b581518b7947414bca7
sensssz <hjmsens@gmail.com>
2016-05-20 13:13:58 -0400

    Upgrade to unique lock in get

--- src/lrucache.cc
+++ src/lrucache.cc
@@ -4,6 +4,9 @@
 
 #include "lrucache.h"
 
+using boost::upgrade_lock;
+using boost::upgrade_to_unique_lock;
+
 LRUCache::LRUCache() : LRUCache(CACHE_SIZE) {}
 
 LRUCache::LRUCache(uint64_t capacity_in) : capacity(capacity_in), size(0) {}
@@ -37,6 +40,8 @@ bool LRUCache::get(uint32_t key, string &val) {
     auto map_iter = cache.find(key);
     if (map_iter != cache.end()) {
         val = cache[key].first;
+        upgrade_lock lock(read_lock);
+        upgrade_to_unique_lock write_lock(lock);
         lru_keys.erase(map_iter->second.second);
         lru_keys.push_front(key);
         map_iter->second.second = lru_keys.begin();
