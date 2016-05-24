ef505ffd53990a373790c320b13aec42094e0776
sensssz <hjmsens@gmail.com>
2016-05-24 03:41:29 -0400

    Bug fix

--- src/glakv_server.cc
+++ src/glakv_server.cc
@@ -198,7 +198,7 @@ bool prefetch_or_submit(int sockfd, thread_pool &pool, DB &db, vector<double> &l
     bool prediction_success = false;
     auto iter = prefetch_tasks.begin();
     while (iter != prefetch_tasks.end()) {
-        unique_lock task_lock((*iter)->task_mutex);
+        unique_lock<mutex> task_lock((*iter)->task_mutex);
         if ((*iter)->key == key) {
             prediction_success = true;
             prediction_hit++;
