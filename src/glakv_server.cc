9125fa1331d276c1387745312b484d92053cc3e4
sensssz <hjmsens@gmail.com>
2016-09-12 11:30:37 -0400

    Fix bug in calculating total number of runs.

--- src/glakv_server.cc
+++ src/glakv_server.cc
@@ -402,9 +402,9 @@ int main(int argc, char *argv[])
                     for (auto latency : latencies) {
                         sum += latency;
                     }
-                    cout << sum / latencies.size() << "," << (latencies.size() + prefetch_hit) << "," <<
-                            prediction_hit / (latencies.size() + prefetch_hit) << "," <<
-                            prefetch_hit / (latencies.size() + prefetch_hit) << endl;
+                    cout << sum / latencies.size() << "," << latencies.size() << "," <<
+                            prediction_hit / latencies.size() << "," <<
+                            prefetch_hit / latencies.size() << endl;
 //                    cout << "Prediction hits: " << prediction_hit << endl;
 //                    cout << "Prefetch hits: " << prefetch_hit << endl;
                     latencies.clear();
