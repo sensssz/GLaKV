b0b6788bfe3ca11f6399cca82816b1b682e25ed3
sensssz <hjmsens@gmail.com>
2016-09-16 10:13:46 -0400

    Change p for each m.

--- src/run.sh
+++ src/run.sh
@@ -56,23 +56,25 @@ rm -f ${output_path}/${exp_name} && touch ${output_path}/${exp_name}
 t=10
 m=3
 c=4
-p=1
 for((think=300;think<=1300;think+=200));
 do
-    if [[ ! -z ${quit+x} ]]; then
-        exit 0
-    fi
-    ssh salat3 /bin/zsh << EOF
-    export LD_LIBRARY_PATH=/home/jiamin/gcc/lib64:/home/jiamin/usr/lib:$LD_LIBRARY_PATH
-    echo -n "${think}," >> ${output_path}/${exp_name}
-    ${db_path}/glakv_server --dir ${db_path}/glakv_home -p ${p} -n 1 -t ${t} >> ${output_path}/${exp_name}&
+    for p in `seq 0 5`;
+    do
+        if [[ ! -z ${quit+x} ]]; then
+            exit 0
+        fi
+        ssh salat3 /bin/zsh << EOF
+        export LD_LIBRARY_PATH=/home/jiamin/gcc/lib64:/home/jiamin/usr/lib:$LD_LIBRARY_PATH
+        echo -n "${think},${p}," >> ${output_path}/${exp_name}
+        ${db_path}/glakv_server --dir ${db_path}/glakv_home -p ${p} -n 1 -t ${t} >> ${output_path}/${exp_name}&
 EOF
-    sleep 2
-    echo -n "${think}," >> ${output_path}/${exp_name}
-    ${db_path}/glakv_client -e -m ${m} -c ${c} -n ${num_exp} -t ${think} >> ${output_path}/${exp_name}
-    if [[ $? -ne 0 ]]; then
-        ssh salat3 "echo '' >> ${output_path}/${exp_name}"
-    fi
-    sleep 1
+        sleep 2
+        echo -n "${think},${p}," >> ${output_path}/${exp_name}
+        ${db_path}/glakv_client -e -m ${m} -c ${c} -n ${num_exp} -t ${think} >> ${output_path}/${exp_name}
+        if [[ $? -ne 0 ]]; then
+            ssh salat3 "echo '' >> ${output_path}/${exp_name}"
+        fi
+        sleep 1
+    done
 done
 
