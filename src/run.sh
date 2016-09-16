dec1d70f1d243a1f7fc576667727f44baaa6b8de
sensssz <hjmsens@gmail.com>
2016-09-16 10:02:13 -0400

    Add commas.

--- src/run.sh
+++ src/run.sh
@@ -64,11 +64,11 @@ do
     fi
     ssh salat3 /bin/zsh << EOF
     export LD_LIBRARY_PATH=/home/jiamin/gcc/lib64:/home/jiamin/usr/lib:$LD_LIBRARY_PATH
-    echo -n "${think}" >> ${output_path}/${exp_name}
+    echo -n "${think}," >> ${output_path}/${exp_name}
     ${db_path}/glakv_server --dir ${db_path}/glakv_home -p ${p} -n 1 -t ${t} >> ${output_path}/${exp_name}&
 EOF
     sleep 2
-    echo -n "${think}" >> ${output_path}/${exp_name}
+    echo -n "${think}," >> ${output_path}/${exp_name}
     ${db_path}/glakv_client -e -m ${m} -c ${c} -n ${num_exp} -t ${think} >> ${output_path}/${exp_name}
     if [[ $? -ne 0 ]]; then
         ssh salat3 "echo '' >> ${output_path}/${exp_name}"
