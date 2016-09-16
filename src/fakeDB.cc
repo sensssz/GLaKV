540f6fd366be0f521f733a99e143bd0739e6bb97
sensssz <hjmsens@gmail.com>
2016-09-16 15:36:22 -0400

    Set get latency to 20.

--- src/fakeDB.cc
+++ src/fakeDB.cc
@@ -18,7 +18,7 @@ using std::endl;
 using std::chrono::microseconds;
 
 const int CONTENTION = 5;
-const int GET_TIME = 1000;
+const int GET_TIME = 20;
 const int PUT_TIME = 150;
 const int DEL_TIME = 100;
 
